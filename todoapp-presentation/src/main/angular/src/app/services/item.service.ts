4bdeb28d712e1c677ff6c9a33e3b84d9fb1c9eda
Alexander Paszek <alexander.paszek@udo.edu>
2022-06-23 12:45:58 +0200

    improve item service and user service, use http client of auth service, remove injection of http clients

--- todoapp-presentation/src/main/angular/src/app/services/item.service.ts
+++ todoapp-presentation/src/main/angular/src/app/services/item.service.ts
@@ -1,17 +1,14 @@
-import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { TodoItem } from '../entities/todo-item';
 import { TodoItemList } from '../entities/todo-item-list';
 import { SessionAuthService } from './session-auth.service';
+import { HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class ItemService {
 
   private _authService: SessionAuthService;
-
-  //MAYBE NEEDED:
-  private headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'});
   
   public get authService(): SessionAuthService {
     return this._authService;
@@ -20,22 +17,21 @@ export class ItemService {
     this._authService = value;
   }
 
-  constructor(private http: HttpClient,authService:SessionAuthService) {
+  constructor(authService:SessionAuthService) {
     console.log("ItemService: created")
     this._authService = authService;
-    this.http = this._authService.getHTTPClient();
   }
 
   createTodoItem(todoItem:TodoItem): Observable<TodoItem> {
     console.log(`ItemService: createTodoItem called for item '${todoItem.title}'`)
-    return this.http.post<any>(`${this._authService.getBaseUrl()}/item`, todoItem.toObject(), {headers: new HttpHeaders()}).pipe(
+    return this._authService.getHTTPClient().post<any>(`${this._authService.getBaseUrl()}/item`, todoItem.toObject(), {headers: new HttpHeaders()}).pipe(
       map(body => TodoItem.fromObject(body))
     );
   }
 
   updateTodoItem(todoItem:TodoItem): Observable<TodoItem> {
     console.log(`ItemService: createTodoItem called for item '${todoItem.title}'`)
-    return this.http.put<any>(`${this._authService.getBaseUrl()}/item`, todoItem.toObject(), {headers: new HttpHeaders()}).pipe(
+    return this._authService.getHTTPClient().put<any>(`${this._authService.getBaseUrl()}/item`, todoItem.toObject(), {headers: new HttpHeaders()}).pipe(
       map(body => TodoItem.fromObject(body))
     );
   }
@@ -47,14 +43,14 @@ export class ItemService {
       "listTitle" : listTitle,
       "itemTitle" : itemTitle
     }
-    return this.http.delete<any>(`${this._authService.getBaseUrl()}/item`, {headers: new HttpHeaders(),params : params}).pipe(
+    return this._authService.getHTTPClient().delete<any>(`${this._authService.getBaseUrl()}/item`, {headers: new HttpHeaders(),params : params}).pipe(
       map(body => TodoItem.fromObject(body))
     );
   }
   
   createTodoItemList(todoItemList:TodoItemList): Observable<TodoItemList> {
     console.log(`ItemService: createTodoItemList called for list '${todoItemList.title}'`)
-    return this.http.post<any>(`${this._authService.getBaseUrl()}/item/list`, todoItemList.toObject(), {headers: new HttpHeaders()}).pipe(
+    return this._authService.getHTTPClient().post<any>(`${this._authService.getBaseUrl()}/item/list`, todoItemList.toObject(), {headers: new HttpHeaders()}).pipe(
       map(body => TodoItemList.fromObject(body))
     );
   }
@@ -63,7 +59,7 @@ export class ItemService {
   readTodoItemList(listTitle:string): Observable<TodoItemList> {
     console.log(`ItemService: readTodoItemList called for list '${listTitle}'`)
     const params = {"listTitle" : listTitle}
-    return this.http.get<any>(`${this._authService.getBaseUrl()}/item/list`, {headers: new HttpHeaders(),params : params}).pipe(
+    return this._authService.getHTTPClient().get<any>(`${this._authService.getBaseUrl()}/item/list`, {headers: new HttpHeaders(),params : params}).pipe(
       map(body => TodoItemList.fromObject(body))
     );
   }
@@ -73,15 +69,22 @@ export class ItemService {
     const params = {
       "listTitle" : listTitle
     }
-    return this.http.delete<any>(`${this._authService.getBaseUrl()}/item/list`, {headers: new HttpHeaders(),params : params}).pipe(
+    return this._authService.getHTTPClient().delete<any>(`${this._authService.getBaseUrl()}/item/list`, {headers: new HttpHeaders(),params : params}).pipe(
       map(body => TodoItemList.fromObject(body))
     );
   }
 
   readAllTodoItemLists(): Observable<TodoItemList[]> {
     console.log(`ItemService: readTodoItemList called`)
-    return this.http.get<any>(`${this._authService.getBaseUrl()}/item/list/all`, {headers: new HttpHeaders()}).pipe(
-      map(body => body.lists.map(obj => { return TodoItemList.fromObject(obj)}))
+    return this._authService.getHTTPClient().get<TodoItemList[]>(`${this._authService.getBaseUrl()}/item/list/all`, {headers: new HttpHeaders()}).pipe(
+      map(body => {
+        console.log("--------------")
+        console.log("Received body on readAllTodoItemLists:", body)
+        console.log("Body as json:", JSON.stringify(body))
+        console.log("typeof:", typeof(body))
+        console.log("--------------")
+        return body.map(obj => { return TodoItemList.fromObject(obj)})
+      })
     );
   }
 
--- todoapp-presentation/src/main/angular/src/app/services/user.service.ts
+++ todoapp-presentation/src/main/angular/src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient,HttpHeaders} from '@angular/common/http';
+import { HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { TodoItem } from '../entities/todo-item';
@@ -18,10 +18,9 @@ export class UserService {
   public get authService(): SessionAuthService {
     return this._authService;
   }
-  constructor(private http: HttpClient,authService:SessionAuthService) {
+  constructor(authService:SessionAuthService) {
     console.log("UserService: created")
     this._authService = authService
-    this.http = this._authService.getHTTPClient();
   }
 
   deleteUserAccount(username:string): Observable<UserAccount> {
@@ -29,29 +28,43 @@ export class UserService {
     const params = {
       "username" : username
     }
-    return this.http.delete<any>(`${this._authService.getBaseUrl()}/user`, {headers: new HttpHeaders(),params : params}).pipe(
+    return this._authService.getHTTPClient().delete<any>(`${this._authService.getBaseUrl()}/user`, {headers: new HttpHeaders(),params : params}).pipe(
       map(body => UserAccount.fromObject(body))
     );
   }
 
   readUserAccount(): Observable<UserAccount> {
     console.log(`UserService: readUserAccount called `)
-    return this.http.get<any>(`${this._authService.getBaseUrl()}/user`, {headers: new HttpHeaders()}).pipe(
+    return this._authService.getHTTPClient().get<any>(`${this._authService.getBaseUrl()}/user`, {headers: new HttpHeaders()}).pipe(
       map(body => UserAccount.fromObject(body))
     );
   }
 
   readAllUserNames(): Observable<string[]> {
     console.log(`UserService: readAllUserNames called`)
-    return this.http.get<any>(`${this._authService.getBaseUrl()}/user/all`, {headers: new HttpHeaders()}).pipe(
-      map(body => body.names )
+    return this._authService.getHTTPClient().get<string[]>(`${this._authService.getBaseUrl()}/user/all`, {headers: new HttpHeaders()}).pipe(
+      map(body => {
+        console.log("--------------")
+        console.log("Received body on readAllUserNames:", body)
+        console.log("Body as json:", JSON.stringify(body))
+        console.log("typeof:", typeof(body))
+        console.log("--------------")
+        return body
+      })
     );
   }
 
   readUserAssignedTodoItems(): Observable<TodoItem[]> {
     console.log(`ItemService: readUserAssignedTodoItems called`)
-    return this.http.get<any>(`${this._authService.getBaseUrl()}/user/items`, {headers: new HttpHeaders()}).pipe(
-      map(body => body.items.map(obj=> {return TodoItem.fromObject(obj)}))
+    return this._authService.getHTTPClient().get<TodoItem[]>(`${this._authService.getBaseUrl()}/user/items`, {headers: new HttpHeaders()}).pipe(
+      map(body => {
+        console.log("--------------")
+        console.log("Received body on readUserAssignedTodoItems:", body)
+        console.log("Body as json:", JSON.stringify(body))
+        console.log("typeof:", typeof(body))
+        console.log("--------------")
+        return body.map(obj=> {return TodoItem.fromObject(obj)})
+      })
     );
   }
 
