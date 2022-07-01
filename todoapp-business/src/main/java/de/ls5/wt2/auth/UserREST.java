65f71799376ae507cdaca91189dd8113c755f058
Niklas Schrötler <privat@nschroetler.de>
2022-07-01 16:20:28 +0200

    UserREST: Adapted methods to use new DB structure

--- todoapp-business/src/main/java/de/ls5/wt2/auth/UserREST.java
+++ todoapp-business/src/main/java/de/ls5/wt2/auth/UserREST.java
@@ -27,27 +27,12 @@ public class UserREST {
     @Autowired
     private EntityManager entityManager;
 
-    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity<?> getProfile() {
-
-        final Subject subject = SecurityUtils.getSubject();
-
-        if (!subject.isAuthenticated()) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-
-        return ResponseEntity.ok(subject.getPrincipal().toString());
-    }
-
-    @PostMapping(consumes=MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<DBUserAccount> createUser(@RequestBody final DBUserAccount param) {
         String username = param.getUsername();
+
         List<DBUserAccount> users = this.entityManager
-                .createQuery(
-                        "SELECT u from DBUserAccount u WHERE u.username = :username",
-                        DBUserAccount.class
-                )
+                .createQuery("SELECT u FROM DBUserAccount u WHERE u.username = :username", DBUserAccount.class)
                 .setParameter("username", username)
                 .getResultList();
 
@@ -59,11 +44,9 @@ public class UserREST {
         acc.setUsername(param.getUsername());
         acc.setPassword(param.getPassword());
         acc.setRegistrationDate(new Date());
-        acc.setUserRole(UserRole.REGULAR.toString());
+        acc.setUserRole(UserRole.REGULAR);
 
         this.entityManager.persist(acc);
-
-        acc.setPassword("x");
 
         return new ResponseEntity<>(acc, HttpStatus.CREATED);
     }
@@ -71,65 +54,72 @@ public class UserREST {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<DBUserAccount> readUser() {
         final Subject subject = SecurityUtils.getSubject();
+
         if (subject == null || !subject.isAuthenticated()) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
+
         final String userId = subject.getPrincipal().toString();
         List<DBUserAccount> users = this.entityManager
-                .createQuery(
-                        "SELECT u from DBUserAccount u WHERE u.username = :username",
-                        DBUserAccount.class
-                )
+                .createQuery("SELECT u from DBUserAccount u WHERE u.username = :username", DBUserAccount.class)
                 .setParameter("username", userId)
                 .getResultList();
 
         if (users == null || users.size() != 1) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+
+        // ToDo: Allow this for the user themselves or for admin
+
         DBUserAccount account = users.get(0);
-        account.setPassword("x");
         return ResponseEntity.ok(account);
     }
 
-    @DeleteMapping(params = { "username" },
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(params = {"username"}, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<DBUserAccount> deleteUser(@RequestParam final String username) {
         final Subject subject = SecurityUtils.getSubject();
         if (subject == null || !subject.isAuthenticated()) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
-        subject.checkRole(UserRole.ADMIN.toString());
+        subject.checkRole(UserRole.ADMIN);
 
-//        subject.checkRole("admin");
-
-        DBUserAccount user = this.entityManager.createQuery("SELECT u from DBUserAccount u WHERE u.username = :username ",DBUserAccount.class)
+        DBUserAccount user = this.entityManager
+                .createQuery("SELECT u from DBUserAccount u WHERE u.username = :username ", DBUserAccount.class)
                 .setParameter("username", username)
                 .getSingleResult();
-        if (user==null){
+
+        if (user == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        List<DBTodoItem> itemassi = this.entityManager.createQuery("SELECT u from DBTodoItem u WHERE u.assignee = :username ",DBTodoItem.class).
-                setParameter("username", username).getResultList();
-        for (int i=0 ; i<itemassi.size();i++) {
-            itemassi.get(i).setAssignee("None");
+
+        List<DBTodoItem> itemassi = this.entityManager
+                .createQuery("SELECT u from DBTodoItem u WHERE u.assignee = :username ", DBTodoItem.class)
+                .setParameter("username", username)
+                .getResultList();
+
+        for (DBTodoItem dbTodoItem : itemassi) {
+            dbTodoItem.setAssignee(null);
         }
-        List<DBTodoItem> itemcre = this.entityManager.createQuery("SELECT u from DBTodoItem u WHERE u.creator = :username ",DBTodoItem.class).
-                setParameter("username", username).getResultList();
-        for (int j=0 ; j<itemcre.size();j++) {
-            itemcre.get(j).setCreator("None");
-        }
-        List<DBTodoItemList> itemlistcre = this.entityManager.createQuery("SELECT u from DBTodoItemList u WHERE u.creator = :username ",DBTodoItemList.class).
-                setParameter("username", username).getResultList();
-        for (int z=0 ; z<itemlistcre.size();z++) {
-            itemlistcre.get(z).setCreator("None");
-        }
+
+        List<DBTodoItem> itemcre = this.entityManager
+                .createQuery("SELECT u from DBTodoItem u WHERE u.creator = :username ", DBTodoItem.class)
+                .setParameter("username", username)
+                .getResultList();
+
+        List<DBTodoItemList> itemlistcre = this.entityManager
+                .createQuery("SELECT u from DBTodoItemList u WHERE u.creator = :username ", DBTodoItemList.class)
+                .setParameter("username", username)
+                .getResultList();
+
         this.entityManager.remove(user);
-        return new ResponseEntity<>(user,HttpStatus.ACCEPTED);
+        this.entityManager.remove(itemcre);
+        this.entityManager.remove(itemlistcre);
+
+        return new ResponseEntity<>(user, HttpStatus.ACCEPTED);
     }
 
-    @GetMapping(path = "items",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "items", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<DBTodoItem>> readAssignedItems() {
         final Subject subject = SecurityUtils.getSubject();
         if (subject == null || !subject.isAuthenticated()) {
@@ -137,46 +127,43 @@ public class UserREST {
         }
         final String userId = subject.getPrincipal().toString();
 
-        DBUserAccount account = this.entityManager.createQuery("SELECT u from DBUserAccount u WHERE u.username = :userId ",DBUserAccount.class).
-                setParameter("userId",userId ).getSingleResult();
+        DBUserAccount account = this.entityManager
+                .createQuery("SELECT u from DBUserAccount u WHERE u.username = :userId ", DBUserAccount.class)
+                .setParameter("userId", userId)
+                .getSingleResult();
+
         String username = account.getUsername();
-        List <DBTodoItem> listItems = this.entityManager.createQuery("SELECT u from DBTodoItem u WHERE u.assignee = :username ",DBTodoItem.class).
-                setParameter("username", username).getResultList();
-        if (listItems == null){
+
+        List<DBTodoItem> listItems = this.entityManager
+                .createQuery("SELECT u from DBTodoItem u WHERE u.assignee = :username ", DBTodoItem.class)
+                .setParameter("username", username)
+                .getResultList();
+
+        if (listItems == null) {
             listItems = new ArrayList<DBTodoItem>();
         }
 
         //it is ok to return empty list, means: no items assigned
-        return new ResponseEntity<>(listItems,HttpStatus.OK);
-
+        return new ResponseEntity<>(listItems, HttpStatus.OK);
     }
 
-    @GetMapping(path = "all",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<String>> readAllUsernames() {
+    @GetMapping(path = "all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List> readAllUsernames() {
         final Subject subject = SecurityUtils.getSubject();
         if (subject == null || !subject.isAuthenticated()) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
-        subject.checkRole(UserRole.ADMIN.toString());
+        subject.checkRole(UserRole.ADMIN);
 
-        final CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
-        final CriteriaQuery<DBUserAccount> query = builder.createQuery(DBUserAccount.class);
+        List<DBUserAccount> result = this.entityManager
+                .createQuery("SELECT u.id, u.username from DBUserAccount u")
+                .getResultList();
 
-        final Root<DBUserAccount> from = query.from(DBUserAccount.class);
+        if (result == null) {
+            result = new ArrayList<DBUserAccount>();
+        }
 
-        final Order order = builder.desc(from.get(DBUserAccount_.username));
-
-        query.select(from).orderBy(order);
-
-        final List<String> result = this.entityManager
-                .createQuery(query)
-                .getResultList()
-                .stream()
-                .map(DBUserAccount::getUsername)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(result);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
