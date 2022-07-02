69a10cc102e44bd69cd0ef976eefb7bbb1889221
Alexander Paszek <alexander.paszek@udo.edu>
2022-07-02 22:29:02 +0200

    -add deauth guard

--- todoapp-presentation/src/main/angular/src/app/app-routing.module.ts
+++ todoapp-presentation/src/main/angular/src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { ProfileComponent } from './components2/profile/profile.component';
 import { AuthentificationComponent } from './components2/authentification/authentification.component';
 import { RegistrationComponent } from './components2/registration/registration.component';
 import { AuthGuard } from './services/auth.guard';
+import { DeAuthGuard } from './services/de-auth.guard';
 
 const routes: Routes = [
-  { path: 'auth', component: AuthentificationComponent},
-  { path: 'register', component: RegistrationComponent},
+  { path: 'auth', component: AuthentificationComponent,canActivate:[DeAuthGuard]},//
+  { path: 'register', component: RegistrationComponent,canActivate:[DeAuthGuard]},//
   { path: 'dashboard', component: DashboardComponent,canActivate:[AuthGuard]},//
   { path: 'profile', component: ProfileComponent,canActivate:[AuthGuard]},//
   { path: '', redirectTo:"/dashboard",pathMatch:"prefix"}
