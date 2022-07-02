d8e2cae822ecbd0fedcaeb473f72ef39dda5f2d2
Niklas Schrötler <privat@nschroetler.de>
2022-07-02 23:58:32 +0200

    Logout: Added deleting JSESSIONID

--- todoapp-business/src/main/java/de/ls5/wt2/conf/auth/LogoutFilterWithoutRedirect.java
+++ todoapp-business/src/main/java/de/ls5/wt2/conf/auth/LogoutFilterWithoutRedirect.java
@@ -2,10 +2,19 @@ package de.ls5.wt2.conf.auth;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.web.filter.authc.LogoutFilter;
+import org.apache.shiro.web.util.WebUtils;
 
 public class LogoutFilterWithoutRedirect extends LogoutFilter {
+    @Override
+    protected boolean preHandle(ServletRequest request, ServletResponse response) {
+        HttpServletResponse httpServletResponse = WebUtils.toHttp(response);
+        httpServletResponse.setStatus(200);
+        httpServletResponse.setHeader("Set-Cookie", "JSESSIONID=EMPTY; expires=Thu, 01 Jan 1970 00:00:00 GMT");
+        return false;
+    }
 
     @Override
     protected void issueRedirect(ServletRequest request, ServletResponse response, String redirectUrl) {
